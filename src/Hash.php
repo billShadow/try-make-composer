c1c7be63905eb13020086537e01bb19a3901a951
xiaobin <v_xiaobin@baidu.com>
2019-12-30 19:00:48 +0800

    init && composer package

---
+++ src/Hash.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Description：类说明
+ * Author: v_xiaobin@baidu.com
+ * Date: 2019-12-30
+ * Time: 18:00
+ */
+
+namespace Bill\Hash;
+
+class Hash {
+    public function desc()
+    {
+        echo "hash action";
+    }
+}
